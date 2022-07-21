b60c8881db609acc7331511130866e8043109664
chenmoonmo <chenmoonmo@gmail.com>
2022-07-22 01:39:53 +0800

    fix: 添加transition

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,50 +1,50 @@
-import { Skeleton, Tag, TagLabel } from "@chakra-ui/react";
-import { useMount, useUpdateEffect } from "ahooks";
-import { PostCard } from "components";
-import { NextPage } from "next";
-import { useRouter } from "next/router";
-import { ReactElement, useEffect, useState } from "react";
-import { getpostsListData } from "slices/posts";
-import { useAppDispatch, useAppSelector } from "utils";
+import { ScaleFade, Skeleton, Tag, TagLabel } from '@chakra-ui/react'
+import { useMount, useUpdateEffect } from 'ahooks'
+import { PostCard } from 'components'
+import { NextPage } from 'next'
+import { useRouter } from 'next/router'
+import { ReactElement, useEffect, useState } from 'react'
+import { getpostsListData } from 'slices/posts'
+import { useAppDispatch, useAppSelector } from 'utils'
 
-import styles from "./index.module.scss";
+import styles from './index.module.scss'
 
-import { intersection, remove, cloneDeep } from "lodash";
+import { intersection, remove, cloneDeep } from 'lodash'
 
 const Posts: NextPage = (): ReactElement => {
-  const router = useRouter();
-  const dispatch = useAppDispatch();
-  const { isLoading, list, tags } = useAppSelector((state) => state.posts);
+  const router = useRouter()
+  const dispatch = useAppDispatch()
+  const { isLoading, list, tags } = useAppSelector((state) => state.posts)
 
-  const [currentTags, setTags] = useState<string[]>(tags);
+  const [currentTags, setTags] = useState<string[]>(tags)
 
   const handleToPosts = (id: string) => {
-    router.push(`/posts/${id}`);
-  };
+    router.push(`/posts/${id}`)
+  }
 
   const handleFilte = (item: string) => {
     if (currentTags.includes(item)) {
       setTags((preTags) => {
-        let cloneTags = cloneDeep(preTags);
-        remove(cloneTags, (x) => x === item);
-        return cloneTags;
-      });
+        let cloneTags = cloneDeep(preTags)
+        remove(cloneTags, (x) => x === item)
+        return cloneTags
+      })
     } else {
       setTags((preTags) => {
-        return [...preTags, item];
-      });
+        return [...preTags, item]
+      })
     }
-  };
+  }
 
   useMount(() => {
-    dispatch(getpostsListData());
-  });
+    dispatch(getpostsListData())
+  })
 
   useUpdateEffect(() => {
     if (currentTags.length === 0) {
-      setTags(tags);
+      setTags(tags)
     }
-  }, [tags]);
+  }, [tags])
 
   return (
     <main className={styles.container}>
@@ -54,7 +54,7 @@ const Posts: NextPage = (): ReactElement => {
             className={styles.tag}
             key={item}
             size="sm"
-            colorScheme={currentTags?.includes(item) ? "messenger" : "gray"}
+            colorScheme={currentTags?.includes(item) ? 'messenger' : 'gray'}
             onClick={() => handleFilte(item)}
           >
             <TagLabel>{item}</TagLabel>
@@ -72,17 +72,20 @@ const Posts: NextPage = (): ReactElement => {
             <Skeleton height={180}></Skeleton>
           </>
         ) : (
-          list
-            ?.filter((item) => {
-              return intersection(item.tags, currentTags).length > 0;
-            })
-            ?.map((data: any) => (
-              <PostCard key={data.id} data={data} onClick={handleToPosts} />
-            ))
+          list?.map((data: any) => (
+            <ScaleFade
+              key={data.id}
+              initialScale={0.9}
+              in={intersection(data.tags, currentTags).length > 0}
+              unmountOnExit
+            >
+              <PostCard data={data} onClick={handleToPosts} />
+            </ScaleFade>
+          ))
         )}
       </div>
     </main>
-  );
-};
+  )
+}
 
-export default Posts;
+export default Posts
