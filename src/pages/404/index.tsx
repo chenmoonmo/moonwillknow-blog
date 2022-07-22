d6197c7a57156f7bdb9a1631c52954bea994269c
chenmoonmo <chenmoonmo@gmail.com>
2022-07-23 01:27:45 +0800

    fix: 404页面调整

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,22 +1,28 @@
-import { NextPage } from "next";
-import styles from "./index.module.scss";
+import { NextPage } from 'next'
+import styles from './index.module.scss'
 
 const NotFound: NextPage = () => {
-  const emoji = ["🌑", "🌒", "🌓", "🌖", "🌕", "🌔", "🌗", "🌘"];
-  const words = new Array(60).fill("");
+  const emoji = ['🌑', '🌒', '🌓', '🌔', '🌕', '🌖', '🌗', '🌘']
+  const words = new Array(60).fill('')
   return (
     <div className={styles.container}>
       <div className={styles.textLayer}>
         {words.map((item, index) => {
+          const count = index * 3
           return (
             <span key={index}>
-              4<span className={styles.text}>{emoji[(index % emoji.length) - 1]}</span>4N<span className={styles.text}>{emoji[((2 * index) % emoji.length) - 1]}</span>tF<span className={styles.text}>{emoji[((3 * index) % emoji.length) - 1]}</span>und.
+              4<span className={styles.text}>{emoji[(1 + count) % 8]}</span>
+              4N
+              <span className={styles.text}>{emoji[(2 + count) % 8]}</span>
+              tF
+              <span className={styles.text}>{emoji[(3 + count) % 8]}</span>
+              und.
             </span>
-          );
+          )
         })}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default NotFound;
+export default NotFound
