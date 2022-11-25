54a4c8c8f4954be462390f3001cd617b6847c877
chenmoonmo <chenmoonmo@gmail.com>
2022-11-25 13:32:07 +0800

    fix: 首图访问问题

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -6,7 +6,7 @@ import { FC, ReactElement } from 'react';
 const Banner: FC = (): ReactElement => {
   return (
     <div className={styles.container}>
-      <Image src='/images/cat.jpg' alt='' layout='fill' objectFit='cover' quality={100} />
+      <Image src='/images/cat.JPG' alt='' layout='fill' objectFit='cover' quality={100} />
       <motion.div
         className={styles.text}
         animate={{
